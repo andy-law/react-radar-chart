96b5f4323a0fd643bd54c1ef11440fad485d34fd
Andy Law <mail@andylaw.info>
2016-01-16 17:50:19 +0000

    Initial files for component - jsx and scss

---
+++ src/react-radar-chart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './react-radar-chart.scss';
+
+class ReactRadarChart extends React.Component {
+
+  render() {
+    return (
+      <svg className='react-radar-chart'>
+
+      </svg>
+    );
+  }
+}
