a0e2cc8075035f9cdc995f933710279d85838fc6
Andy Law <andy.law@quantumblack.com>
2016-01-26 16:54:57 +0000

    Set scales and default proptypes

--- src/react-radar-chart.jsx
+++ src/react-radar-chart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import d3_scale from 'd3-scale';
 
 import ReactRadarChartLine from './react-radar-chart-line';
 
@@ -9,16 +10,35 @@ class ReactRadarChart extends React.Component {
   constructor(props) {
     super(props);
     this._defaultSize = '100%';
+    this._radiusScale = d3_scale.linear();
+    this._angleScale = d3_scale.linear()
+      range([0, Math.PI * 2]);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.keys && nextProps.keys.length && nextProps.keys.length !== this.props.keys.length) {
+      this.setScaleDomain(this._angleScale, 0, nextProps.keys.length);
+    }
+  }
+
+  dataUpdatedHandler() {
+    if (this.props.outerRadius) {
+      this._radiusScale
+        .domain()
+    }
+    if (this.props.keys && this.props.keys.length) {
+      this._angleScale.domain([0, this.props.keys.length]);
+    }
   }
 
   getLineElems() {
     if (!this.props.values) {
-      return <g></g>;
-    } else {
-      return this.props.values.map((d, i) => {
-        return <ReactRadarChartLine key={ i } />;
-      });
+      return '';
     }
+
+    return this.props.values.map((d, i) => {
+      return <ReactRadarChartLine key={ i } />;
+    });
   }
 
   render() {
@@ -33,5 +53,16 @@ class ReactRadarChart extends React.Component {
     );
   }
 }
+
+ReactRadarChart.propTypes = {
+  svgSize: React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    React.PropTypes.string
+  ]),
+  keys: React.PropTypes.array,
+  values: React.PropTypes.array,
+  innerRadius: React.PropTypes.number,
+  outerRadius: React.PropTypes.number
+};
 
 export default ReactRadarChart;
